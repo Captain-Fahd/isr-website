ad0b3c8a8185916dd0cd6685b0cc169732e8a088
Captain-Fahd <omarfahd06@gmail.com>
2026-07-07 21:37:23 +1000

    Implement Admin authentication; Update API.md

--- backend/routes/auth.ts
+++ backend/routes/auth.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { signIn } from "../controllers/authController";
+import { signIn, getMe } from "../controllers/authController";
+import { checkAuth } from "../middleware/checkAuth";
 
 const router = Router();
 
-router.post('/signin', signIn);
+router.post("/signin", signIn);
+router.get("/me", checkAuth, getMe);
 
 export default router;
