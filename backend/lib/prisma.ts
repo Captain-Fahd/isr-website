300c4a7600290abe8fbbe6bfe4fb8fa5b3376790
Captain-Fahd <omarfahd06@gmail.com>
2026-07-11 21:35:30 +1000

    Implement fixes for adding images

--- backend/lib/prisma.ts
+++ backend/lib/prisma.ts
@@ -1,3 +1,8 @@
 import {PrismaClient} from "../generated/prisma";
+import pg from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-export const prisma = new PrismaClient();
+
+const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg(pool);
+export const prisma = new PrismaClient({adapter});
