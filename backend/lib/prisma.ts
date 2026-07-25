ac20f7f64a12309774c03219c2ec5ab6b3aaae35
Captain-Fahd <omarfahd06@gmail.com>
2026-07-25 20:53:53 +1000

    Fix: relative prisma path && tsconfig

--- backend/lib/prisma.ts
+++ backend/lib/prisma.ts
@@ -1,5 +1,5 @@
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../generated/prisma";
+import { PrismaClient } from "../dist/generated/prisma";
 
 const connectionString = process.env.DATABASE_URL;
 if (!connectionString) {
