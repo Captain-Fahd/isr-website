65002825e9329ec5e657ad5c788d843db5be307d
Captain-Fahd <omarfahd06@gmail.com>
2026-08-06 10:07:40 +1000

    Removing theisr-backend and frontend folders.

--- theisr-backend/isr-website-main/backend/routes/weather.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { getMelbourneWeather } from '../controllers/weatherController';
-
-const router = Router();
-
-router.get('/', getMelbourneWeather);
-
-export default router;
